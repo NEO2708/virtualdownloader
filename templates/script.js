28b56d7247774280f94adaf9c14f9ebb71684793
NEO2708 <parthr.2608@gmail.com>
2023-07-07 17:01:59 +0530

    update0.0.0.0.2

--- templates/script.js
+++ templates/script.js
@@ -22,10 +22,137 @@ igdownload.addEventListener("click", () => {
     }
     else {
         igdownload.innerHTML = "Please Wait ..."
-        loader.style.display = "flex";
         var url = "https://web-production-597b.up.railway.app/insta?link=" + linkf;
-        form.style.display="none"
-        execute(url)
+
+        loader.style.display = "flex";
+        fetch(url, { mode: 'cors', method: "GET" })
+        .then((response) => response.json())
+        .then((data) => {
+
+
+
+            const videos = data.videourls;
+            const images = data.imageurls;
+            if (videos.length == 1) {
+                loader.style.display = "none";
+                form.style.display = "none"
+
+
+                content.innerHTML += `
+                <div class="dcard">
+                    <div class="profilerr">
+
+
+                    <div class="colname">
+                    <span class="punun" >${data.username}</span>
+                    <span class="pun" >${data.name}</span>
+                        </div>
+
+                    </div>
+                    <div class="cover">
+                        <video src="${videos[0]}" controls ></video>
+                    </div>
+                    <div class="dowlik">
+                        <a href="${videos[0]}">Download Video</a>
+                    </div>
+                </div>`
+                red.style.display = "flex"
+            }
+            if (images.length == 1) {
+                loader.style.display = "none";
+                form.style.display = "none"
+
+
+                content.innerHTML += `
+                 <div class="dcard">
+                <div class="profilerr">
+
+                    <div class="colname">
+                            <span class="punun" >${data.username}</span>
+                            <span class="pun" >${data.name}</span>
+                                </div>
+                </div>
+                <div class="cover">
+                    <img src="${images[0]}" alt="expired">
+                </div>
+                <div class="desc">
+                    <span class="pun" >${data.caption}</span>
+                </div>                    
+                <div class="dowlik">
+                    <a href="${images[0]}">Download Image</a>
+                </div>
+                </div> 
+
+            `
+            red.style.display="flex"
+
+            }
+            if (videos.length > 1) {
+
+                for (let index = 0; index < videos.length; index++) {
+                    loader.style.display = "none";
+                form.style.display = "none"
+
+
+                    content.innerHTML += `
+                    <div class="dcard">
+                        <div class="profilerr">
+
+
+                            <div class="colname">
+                            <span class="punun" >${data.username}</span>
+                            <span class="pun" >${data.name}</span>
+                                </div>
+                            
+                        </div>
+                        <div class="cover">
+                            <video src="${videos[index]}" controls ></video>
+                        </div>
+                        <div class="dowlik">
+                            <a href="${videos[index]}">Download Video</a>
+                        </div>
+                    </div> 
+`
+                    red.style.display = "flex"
+
+                }
+            }
+            if (images.length > 1) {
+                for (let index = 0; index < images.length; index++) {
+                    loader.style.display = "none";
+                form.style.display = "none"
+
+
+                    content.innerHTML += `
+                <div class="dcard">
+                    <div class="profilerr">
+
+                        <div class="colname">
+                            <span class="punun" >${data.username}</span>
+                            <span class="pun" >${data.name}</span>
+                                </div>
+                    </div>
+                    <div class="cover">
+                        <img src="${images[index]}" alt="expired">
+                    </div>
+                    <div class="dowlik">
+                        <a href="${images[index]}">Download Image</a>
+                    </div>
+                </div> 
+
+            `
+                    red.style.display = "flex"
+
+
+                }
+            }
+
+        }).catch(error =>
+            igdownload.innerHTML = "Download ",
+
+            content.innerHTML = `<h1 class="err" >Check if the Account is Not PRIVATE and try again </h1>`,
+            red.style.display = "flex")
+
     }
 
 
@@ -148,13 +275,11 @@ function execute(url) {
                 }
             }
 
-        }
-
-        ).catch(error =>
+        }).catch(error =>
             igdownload.innerHTML = "Download ",
 
             content.innerHTML = `<h1 class="err" >Check if the Account is Not PRIVATE and try again </h1>`,
-            red.style.display = "flex"
+            red.style.display = "flex")
 
-        )
+        
 }
