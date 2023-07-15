5158454cfbb7bfce5e4d7aead9156f925f53cf90
NEO2708 <parthr.2608@gmail.com>
2023-07-15 17:41:00 +0530

    profile_test-1

---
+++ home/downloadprof.py
@@ -0,0 +1,20 @@
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+import instaloader
+@api_view()
+
+def downloadprof(request):
+    L = instaloader.Instaloader()
+    stream=[]
+    username = request.GET['link']
+    posts = instaloader.Profile.from_username(L.context, username).get_posts()
+    users = set()
+    for post in posts:
+        # L.download_post(post, 'msf.inc')
+        # users.add(post.owner_profile)
+        js={"Url":post.url,"Caption":post.caption,"Date":post.date}
+        stream.append(js)
+
+    else:
+        print("{} from {} skipped.".format(post, post.owner_profile))
+    return Response(stream)
--- home/urls.py
+++ home/urls.py
@@ -2,10 +2,12 @@
 from django.urls import path
 from home import igdownloader
 from home import home
+from home import downloadprof
 
 
 urlpatterns = [
     path('insta',igdownloader.InstaDownloader ),
     path('homepage',home.homepage),
     path('',home.homepage),
+    path('igprofile',downloadprof.downloadprof)
 ]
